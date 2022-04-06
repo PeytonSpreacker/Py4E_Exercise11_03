994120a147dca6506a5582a1ae2699ab627110a4
PeytonSpreacker <peyton@spreacker.com>
2022-04-06 11:06:17 -0500

    final commit

--- 11_03_sample.py
+++ 11_03_sample.py
@@ -1,8 +1,19 @@
 # imports regular expression library
 import re
 
+# starting sum value
+sum = 0
+
 # open the appropriate file from a user input
 user_command = input('Select a file: ')
 if len(user_command)<1:
-    handle = open('regex_sum_1501839.txt')
+    handle = open('sample_regex_sum_42.txt')
 
+# extract number strings from the file and add them
+for line in handle:
+    line.rstrip()
+    num = re.findall('([0-9]+)', line)
+    for i in num:
+        inum = int(i)
+        sum = sum + inum
+print(sum)
